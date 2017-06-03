132dcbf23956feb8103757e6248e841490445b09
dsteiner <diehtard.steiner@gmail.com>
2017-06-03 18:13:46 +0100

    bug corrected

--- shell-scripts/run_tr_add_one.sh
+++ shell-scripts/run_tr_add_one.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/home/dsteiner/apps/data-integration/pan.sh -file=../etl/tr_add_one.ktr
+/home/dsteiner/apps/data-integration/pan.sh -file=etl/tr_add_one.ktr -level=Detailed
 RES=$?
 exit $RES
